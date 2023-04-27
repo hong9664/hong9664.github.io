6d41fa7ccd5f3d9c5520c7403c02c752adece95e
hong9664 <hdklem@naver.com>
2023-04-27 10:13:02 +0900

    기울기

--- dist/script.js
+++ dist/script.js
@@ -258,6 +258,22 @@ window.addEventListener("touchend", (event) => {
   cursor.y = 0;
 });
 
+/**모바일 기울기 이벤트 */
+window.addEventListener(
+  "deviceorientation",
+  (event) => {
+    const x = event.beta; // 디바이스의 x축 기울기
+    const y = event.gamma; // 디바이스의 y축 기울기
+
+    // x, y 값을 이용하여 적절한 움직임을 적용합니다.
+    // 예를 들어, 아래와 같이 움직일 수 있습니다.
+    const moveX = x * 2;
+    const moveY = y * 2;
+    element.style.transform = "translate(" + moveX + "px, " + moveY + "px)";
+  },
+  true
+);
+
 /**
  * Renderer
  */
